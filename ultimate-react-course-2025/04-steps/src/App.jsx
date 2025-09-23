22f87fd4fe8dc6abffb3a7c4c9fd187b2ca79ef9
yuzhang2101 <yuzhangfish25@gmail.com>
2025-09-23 13:24:14 +0800

    add steps practice

--- ultimate-react-course-2025/04-steps/src/App.jsx
+++ ultimate-react-course-2025/04-steps/src/App.jsx
@@ -11,24 +11,43 @@ function App() {
   ];
 
   const [step, setStep] = useState(1);
-  return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
 
-      <h3 className="message">{messages[step - 1]}</h3>
-      <div className="buttons">
-        <button disabled={step == 1} onClick={() => setStep(step - 1)}>
-          <span>Previous</span>
-        </button>
-        <button disabled={step == 3} onClick={() => setStep(step + 1)}>
-          <span>Next</span>
-        </button>
-      </div>
-    </div>
+  const [isOpen, setIsOpen] = useState(true);
+  const handlePrevious = () => {
+    if (step > 1) setStep(step - 1);
+  };
+  const handleNext = () => {
+    if (step < 3) setStep(step + 1);
+  };
+  const handleClose = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <h3 className="message">{messages[step - 1]}</h3>
+          <div className="buttons">
+            <button disabled={step == 1} onClick={handlePrevious}>
+              <span>Previous</span>
+            </button>
+            <button disabled={step == 3} onClick={handleNext}>
+              <span>Next</span>
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
